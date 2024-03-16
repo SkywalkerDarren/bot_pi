d3cc4db486697346d5f173eed1e35930d5cab652
SkywalkerDarren <yh83533010yh2@gmail.com>
2024-03-16 16:55:36 +0800

    init

--- test/test_keyword.py
+++ test/test_keyword.py
@@ -2,8 +2,16 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_azure(self):
+        from keyword_recognize_service.azure_keyword_reconizer import AzureKeywordRecognizer
+        AzureKeywordRecognizer().recognize()
+
+    def test_oww(self):
+        # import openwakeword.utils
+        # openwakeword.utils.download_models()
+
+        from keyword_recognize_service.open_wakeup_word_recognizer import OpenWakeupWordRecognizer
+        OpenWakeupWordRecognizer().recognize()
 
 
 if __name__ == '__main__':
